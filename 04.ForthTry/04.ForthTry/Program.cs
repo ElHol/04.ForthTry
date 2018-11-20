fbb860aecb29a5ae8186af701df17cb1646f4b95
Maria1609 <maria201115@gmail.com>
2018-11-20 22:54:33 +0200

    Start the C# project.

---
+++ 04.ForthTry/04.ForthTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.ForthTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
